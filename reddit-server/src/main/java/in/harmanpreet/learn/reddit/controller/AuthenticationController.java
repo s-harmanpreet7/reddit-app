27b084e52d25749eca59c7e758c1c439f21f2fce
Harmanpreet Singh <59172539+singh-harmanpreet@users.noreply.github.com>
2021-06-20 00:21:20 +0530

    feat: b-4 | implemented signup user endpoint

--- reddit-server/src/main/java/in/harmanpreet/learn/reddit/controller/AuthenticationController.java
+++ reddit-server/src/main/java/in/harmanpreet/learn/reddit/controller/AuthenticationController.java
@@ -1,17 +1,26 @@
 package in.harmanpreet.learn.reddit.controller;
 
 import in.harmanpreet.learn.reddit.dto.RegisterRequest;
+import in.harmanpreet.learn.reddit.service.AuthService;
+import lombok.AllArgsConstructor;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import static org.springframework.http.HttpStatus.OK;
+
 @RestController
 @RequestMapping("/api/auth")
+@AllArgsConstructor
 public class AuthenticationController {
 
-    @PostMapping("/signup")
-    public void signup(@RequestBody RegisterRequest registerRequest) {
+    private final AuthService authService;
 
+    @PostMapping("/signup")
+    public ResponseEntity<String> signup(@RequestBody RegisterRequest registerRequest) {
+        authService.signup(registerRequest);
+        return new ResponseEntity<>("User registration Successful", OK);
     }
 }
--- reddit-server/src/main/java/in/harmanpreet/learn/reddit/service/AuthService.java
+++ reddit-server/src/main/java/in/harmanpreet/learn/reddit/service/AuthService.java
@@ -3,20 +3,21 @@ package in.harmanpreet.learn.reddit.service;
 import in.harmanpreet.learn.reddit.dto.RegisterRequest;
 import in.harmanpreet.learn.reddit.model.User;
 import in.harmanpreet.learn.reddit.repository.UserRepository;
-import org.springframework.beans.factory.annotation.Autowired;
+import lombok.AllArgsConstructor;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
+import javax.transaction.Transactional;
 import java.time.Instant;
 
 @Service
+@AllArgsConstructor
 public class AuthService {
 
-    @Autowired
-    private PasswordEncoder passwordEncoder;
-    @Autowired
-    private UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
+    private final UserRepository userRepository;
 
+    @Transactional
     public void signup(RegisterRequest registerRequest) {
         User user = new User();
         user.setUsername(registerRequest.getUsername());
@@ -24,6 +25,7 @@ public class AuthService {
         user.setPassword(passwordEncoder.encode(registerRequest.getPassword()));
         user.setCreated(Instant.now());
         user.setEnabled(false);
+
         userRepository.save(user);
     }
 }
