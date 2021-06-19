5161b6f5c0388d26f2c78ad90ccc9490cb015685
Harmanpreet Singh <59172539+singh-harmanpreet@users.noreply.github.com>
2021-06-19 23:23:03 +0530

    config: b-3 | added security config for authentication

---
+++ reddit-server/src/main/java/in/harmanpreet/learn/reddit/config/SecurityConfig.java
@@ -0,0 +1,19 @@
+package in.harmanpreet.learn.reddit.config;
+
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(HttpSecurity httpSecurity) throws Exception {
+        httpSecurity.csrf().disable()
+                .authorizeRequests()
+                .antMatchers("/api/auth/**")
+                .permitAll()
+                .anyRequest()
+                .authenticated();
+    }
+}
