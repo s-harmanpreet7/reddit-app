09c3a084e59c17327a61ed7c4ae017611e31118d
Harmanpreet Singh <59172539+singh-harmanpreet@users.noreply.github.com>
2021-06-29 23:59:39 +0530

    initial: implement comments model and dto
    b-12: implement comments api

---
+++ reddit-server/src/main/java/in/harmanpreet/learn/reddit/model/Comment.java
@@ -0,0 +1,32 @@
+package in.harmanpreet.learn.reddit.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotEmpty;
+import java.time.Instant;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+public class Comment {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotEmpty
+    private String text;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "postId", referencedColumnName = "postId")
+    private Post post;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "userId", referencedColumnName = "userId")
+    private User user;
+
+    private Instant createdDate;
+}
